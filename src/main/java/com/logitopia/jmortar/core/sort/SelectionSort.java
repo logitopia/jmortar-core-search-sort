1a9b2b5aac75990e3e7b95f764be034cf25a7ea1
scheesley <stephen.cheesley@gmail.com>
2019-07-26 22:37:02 +0100

    Initial commit of the selection sort implementation.

---
+++ src/main/java/com/logitopia/jmortar/core/sort/SelectionSort.java
@@ -0,0 +1,19 @@
+package com.logitopia.jmortar.core.sort;
+
+import com.logitopia.jmortar.core.comparator.Comparator;
+
+/**
+ * Sorts the given array of items using a selection sort algorithm.
+ *
+ * @param <T> The type of element being sorted.
+ */
+public class SelectionSort<T> implements Sort<T> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void sort(T[] itemsToBeSorted, Comparator<T> comparator) {
+
+    }
+}
