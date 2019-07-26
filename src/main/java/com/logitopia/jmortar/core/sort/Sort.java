92402182fb941a47bbbfbd3d98f6830c5b87105c
scheesley <stephen.cheesley@gmail.com>
2019-07-26 22:13:37 +0100

    Adding functionality to the sort interface.

--- src/main/java/com/logitopia/jmortar/core/sort/Sort.java
+++ src/main/java/com/logitopia/jmortar/core/sort/Sort.java
@@ -1,4 +1,21 @@
 package com.logitopia.jmortar.core.sort;
 
-public interface Sort {
+import com.logitopia.jmortar.core.comparator.Comparator;
+
+/**
+ * Sort the given list, using the given comparator. The comparator will validate whether or not two components are in
+ * the expected position, relative to each other.
+ *
+ * @param <T> The type of item that we wish to sort.
+ */
+public interface Sort<T> {
+
+    /**
+     * Sort the given items, using the given comparator to validate whether or not the elements are in the expected
+     * position.
+     *
+     * @param itemsToBeSorted The items to be sorted.
+     * @param comparator      The comparator that we wish to use to validate the location of the items.
+     */
+    void sort(T[] itemsToBeSorted, Comparator<T> comparator);
 }
