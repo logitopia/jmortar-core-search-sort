94d55d186bf5a818ae07cfe548ebad0cc5408508
scheesley <stephen.cheesley@gmail.com>
2019-07-28 16:06:29 +0100

    Updating the selection sort so that the initial search for the smallest element works as expected. This has been tested through the use of unit testing.

--- src/main/java/com/logitopia/jmortar/core/sort/SelectionSort.java
+++ src/main/java/com/logitopia/jmortar/core/sort/SelectionSort.java
@@ -2,7 +2,9 @@ package com.logitopia.jmortar.core.sort;
 
 import com.logitopia.jmortar.core.comparator.Comparator;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Sorts the given array of items using a selection sort algorithm.
@@ -11,36 +13,20 @@ import java.lang.reflect.Array;
  */
 public class SelectionSort<T> implements Sort<T> {
 
-    private Class<T> elementType;
-
-    /**
-     * Default Constructor. Creates an instance of the sort, where the consumer defines the "type" of the element
-     * that will be sorted.
-     *
-     * @param elementType The type of the element being sorted.
-     */
-    public SelectionSort(final Class<T> elementType) {
-        if (elementType == null) {
-            throw new IllegalArgumentException("Unable to create the SelectionSort with missing element type.");
-        }
-        this.elementType = elementType;
-    }
-
     /**
      * {@inheritDoc}
      */
     @Override
     public void sort(T[] itemsToBeSorted, Comparator<T> comparator) {
-        // TODO - Look into using a stack here rather than a fixed array ... it could save an extra looping cycle.
-        @SuppressWarnings("unchecked")
-        T[] sorted = (T[]) Array.newInstance(elementType, itemsToBeSorted.length);
+        List<T> toBeSorted = new ArrayList<>(Arrays.asList(itemsToBeSorted));
+        List<T> sorted = new ArrayList<>(toBeSorted.size());
 
         boolean first = true;
         T smallest = null;
 
         // find the smallest
-        for (int i=0; i<itemsToBeSorted.length;i++) {
-            T element = itemsToBeSorted[i];
+        for (int i=0; i<toBeSorted.size();i++) {
+            T element = toBeSorted.get(i);
             if (first) {
                 smallest = element;
                 first = false;
@@ -50,7 +36,9 @@ public class SelectionSort<T> implements Sort<T> {
                 }
             }
         }
-        // TODO - The smallest element has to be removed from the original array....
-        sorted[0] = smallest; // TODO <-- Need to calculate the position in the sorted array length - current index
+
+        sorted.add(smallest);
+
+        System.out.println("DEBUGGING == ");
     }
 }
