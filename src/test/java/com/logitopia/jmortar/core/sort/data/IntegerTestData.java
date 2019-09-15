2f4f8d65576e01425b43dad160b98f253f4e9483
scheesley <stephen.cheesley@gmail.com>
2019-09-15 20:52:14 +0100

    Adding constructors to the IntegerTestData strategy that allow the test to specify how many test entries it would like.

--- src/test/java/com/logitopia/jmortar/core/sort/data/IntegerTestData.java
+++ src/test/java/com/logitopia/jmortar/core/sort/data/IntegerTestData.java
@@ -12,10 +12,26 @@ public class IntegerTestData implements TestData<Integer> {
 
     private Random random = new Random();
 
+    private int maxBound = 1000000;
+
+    /**
+     * Create an instance of the test data that uses a default number of 1m entries.
+     */
+    public IntegerTestData() {
+    }
+
+    /**
+     * Create an instance of the test data that specifies how many elements to generate for the test data list.
+     *
+     * @param maxBound The number of entries we want to create in the result list.
+     */
+    public IntegerTestData(int maxBound) {
+        this.maxBound = maxBound;
+    }
+
     @Override
     public List<Integer> get() {
         List<Integer> result = new ArrayList<>();
-        int maxBound = 50000000;
         IntStream.range(0, maxBound)
                 .forEach(e -> result.add(random.nextInt() & Integer.MAX_VALUE));
         return result;
