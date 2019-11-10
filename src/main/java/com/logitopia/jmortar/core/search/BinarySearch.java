78d6c40d1aed3415537086167d38b9d42f5da616
scheesley <stephen.cheesley@gmail.com>
2019-11-10 14:44:37 +0000

    Initial commit of the Binary Search implementation.

---
+++ src/main/java/com/logitopia/jmortar/core/search/BinarySearch.java
@@ -0,0 +1,11 @@
+package com.logitopia.jmortar.core.search;
+
+/**
+ * A 'binary' implementation of {@link Search} works by taking an <b>ordered</b> data set and identifying which
+ * half of the data set contains the value. It will continue to break the data set down until the value is found.
+ *
+ * <p>The binary search is a good tool for searching through medium and larger data sets. This is because it is an
+ * O(log n) efficiency search. It achieves this by eliminating half the results that don't match each time.</p>
+ */
+public class BinarySearch {
+}
