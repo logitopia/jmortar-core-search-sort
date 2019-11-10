3ee204a8f0d8d2e6e899174f2d1ec6113a2e5443
scheesley <stephen.cheesley@gmail.com>
2019-11-10 14:48:57 +0000

    Adding behaviour definition to the binary search object.

--- src/main/java/com/logitopia/jmortar/core/search/BinarySearch.java
+++ src/main/java/com/logitopia/jmortar/core/search/BinarySearch.java
@@ -1,11 +1,47 @@
 package com.logitopia.jmortar.core.search;
 
+import com.logitopia.jmortar.core.comparator.Comparator;
+
+import java.util.List;
+
 /**
  * A 'binary' implementation of {@link Search} works by taking an <b>ordered</b> data set and identifying which
  * half of the data set contains the value. It will continue to break the data set down until the value is found.
  *
  * <p>The binary search is a good tool for searching through medium and larger data sets. This is because it is an
  * O(log n) efficiency search. It achieves this by eliminating half the results that don't match each time.</p>
+ *
+ * @param <T> The type of element that the search will attempt to locate.
  */
-public class BinarySearch {
+public class BinarySearch<T> implements Search<T> {
+
+    private Comparator<T> equalityComparator;
+
+    /**
+     * Default Constructor. Creates an instance of the {@link Search} with the required equality comparator. The
+     * comparator will verify whether an element in a list is equal to the object being searched for. Using a
+     * separate implementation means that we don't need to rely on the objects {@link Object#equals(Object)}
+     * implementation.
+     *
+     * @param equalityComparator The comparator that this search instance will use.
+     */
+    public BinarySearch(Comparator<T> equalityComparator) {
+        this.equalityComparator = equalityComparator;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int findMatch(List<T> elements, T valueToFind) {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Integer> findMatches(List<T> elements, T valueToFind) {
+        return null;
+    }
 }
