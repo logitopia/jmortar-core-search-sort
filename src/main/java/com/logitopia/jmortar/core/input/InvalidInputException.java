ec834dae32524eb9a3a2ee50164e8cfa7133d748
scheesley <stephen.cheesley@gmail.com>
2020-04-26 21:48:22 +0100

    Adding a domain exception that can be thrown on invalid input.

---
+++ src/main/java/com/logitopia/jmortar/core/input/InvalidInputException.java
@@ -0,0 +1,8 @@
+package com.logitopia.jmortar.core.input;
+
+/**
+ * A type of {@link Exception} indicating that an input has been given to a search or sort, such that a result
+ * cannot be determined from it (i.e. null input).
+ */
+public class InvalidInputException extends Exception {
+}
